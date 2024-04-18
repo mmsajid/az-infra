fd79b51bd0b70226d45d98cfa58fa9aab87decdc
mmsajid <mmsajid@gmail.com>
2024-04-18 12:30:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,15 +16,17 @@ provider "azurerm" {
   features {}
 }
 
+
+
 # Resource Group
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = "${random_pet.prefix.id}-rg"
+  name     = "rg-infra"
 }
 
 # Virtual Network
 resource "azurerm_virtual_network" "my_terraform_network" {
-  name                = "${random_pet.prefix.id}-vnet"
+  name                = "vnet-infra-ukwest-01"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
